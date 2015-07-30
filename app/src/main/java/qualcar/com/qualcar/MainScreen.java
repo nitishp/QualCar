43bc34b43a3d59463a31a9165bab8fe4caacc0cf
nitishp <nitishp@umich.edu>
2015-07-29 17:32:08 -0700

    Added Toolbar

--- app/src/main/java/qualcar/com/qualcar/MainScreen.java
+++ app/src/main/java/qualcar/com/qualcar/MainScreen.java
@@ -1,5 +1,6 @@
 package qualcar.com.qualcar;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
--- app/src/main/java/qualcar/com/qualcar/profile_activity.java
+++ app/src/main/java/qualcar/com/qualcar/profile_activity.java
@@ -5,16 +5,23 @@ package qualcar.com.qualcar;
  */
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class profile_activity extends Activity {
+public class profile_activity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_activity);
+
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(mToolbar);
     }
 
 
